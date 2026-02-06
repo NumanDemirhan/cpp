3a95ac55ac5dd16a8180cafa4745978850e8c0fc
KASTEK67 <83731722+KASTEK67@users.noreply.github.com>
2026-02-06 23:17:02 +0300

    Add files via upload

---
+++ mini_calculator.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int squareNumber(int number)
+{
+    return number * number;
+}
+
+double squareNumber(double number)
+{
+    return number * number;
+}
+
+void resetValue(int &number)
+{
+    number = 0;
+}
+
+int main()
+{
+    int x = 7;
+    double y = 9.8;
+
+    cout << "Integer " << x << " number's square: " << squareNumber(x) << endl;
+    cout << "Double " << y << " number's square: " << squareNumber(y) << endl;
+
+    cout << "x value before reset: " << x << endl;
+
+    resetValue(x);
+
+    cout << "x value after reset: " << x << endl;
+    return 0;
+}
